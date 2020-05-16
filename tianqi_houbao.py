77d5597fd376fa86a5ce940fb11bb9ca057df88a
ddxxx <53335801+ddxxx@users.noreply.github.com>
2020-05-16 09:55:37 +0800

    Update tianqi_houbao.py

--- tianqi_houbao.py
+++ tianqi_houbao.py
@@ -3,13 +3,14 @@ import time
 from bs4 import BeautifulSoup
 import requests
 import pymysql
+import datetime
 import warnings
 from pypinyin import pinyin, lazy_pinyin
 
 # 忽略警告信息
 warnings.filterwarnings("ignore")
 # 连接MySQL
-conn = pymysql.connect(host='192.168.1.108', user='root', passwd='001997', db='weatherfa', port=3306, charset='utf8')
+conn = pymysql.connect(host='localhost', user='root', passwd='001997', db='weatherfa', port=3306, charset='utf8')
 cursor = conn.cursor()
 
 
@@ -31,19 +32,19 @@ def get_month_info(url, city):
                 # if index == 0:
 
                 date = td_list[0].text.strip().replace("\n", "")  # 取每个标签的text信息，并使用replace()函数将换行符删除
+                date = date.replace("年", "-").replace("月", "-").replace("日", "")
                 weather = td_list[1].text.strip().replace("\n", "").split("/")
-                d_weather = weather[0].strip()
-                n_weather = weather[1].strip()
+                weather = weather[0].strip()
                 temp = td_list[2].text.strip().replace("\n", "").split("/")
                 d_temp = int(temp[0].replace("℃", "").strip())
                 n_temp = int(temp[1].replace("℃", "").strip())
-                wind = td_list[3].text.strip().replace("\n", "").split("/")
-                d_wind = wind[0].strip()
-                n_wind = wind[1].strip()
-                print(city, date)
-                cursor.execute('insert ignore into history_weather20(city, date, d_weather, n_weather, '
-                               'd_temp, n_temp, d_wind, n_wind) values(%s, %s, %s, %s, %s, %s, %s, %s)'
-                               , (city, date, d_weather, n_weather, d_temp, n_temp, d_wind, n_wind))
+                owind = td_list[3].text.strip().replace("\n", "").split("/")
+                wind = owind[0].strip()
+
+                print(city, date, weather, d_temp, n_temp, wind)
+                cursor.execute('insert ignore into temp_history(city, date, weather, '
+                               'd_temp, n_temp, wind) values(%s, %s, %s, %s, %s, %s) '
+                               , (city, date, weather, d_temp, n_temp, wind))
     except requests.exceptions.ConnectionError:
         print('ConnectionError -- please wait 3 seconds')
         time.sleep(3)
@@ -57,25 +58,22 @@ def get_month_info(url, city):
 
 if __name__ == '__main__':
     cur = conn.cursor()
-    cur.execute("select citypinyin, city from history_citylist_2")
+    cur.execute("select citypinyin, city from table_citylist_1")
     result = cur.fetchall()  # 获取表中所有数据
     for citypinyin, city in result:
         print(city, citypinyin)
-        for month in range(1, 5):
-            y_m = '2020' + str(month).zfill(2)
-            url = 'http://www.tianqihoubao.com/lishi/' + citypinyin + '/month/' + y_m + '.html'
-            get_month_info(url, city)
+        y_m = str(datetime.datetime.now().year) + str(datetime.datetime.now().month).zfill(2)
+        url = 'http://www.tianqihoubao.com/lishi/' + citypinyin + '/month/' + y_m + '.html'
+        get_month_info(url, city)
+        conn.commit()
 
-            conn.commit()
-        cur.execute("delete from history_citylist where city = '%s'" % city)
-        cur.execute("delete from history_citylist_2 where city = '%s'" % city)
-
-        # for year in range(2019, 2020):
-        #     for month in range(1, 12):
-        #         y_m = str(year) + str(month).zfill(2)
-        #         url = ['http://www.tianqihoubao.com/lishi/' + citypinyin + '/month/' +
-        #                y_m + '.html']
-        #         get_month_info(url, citypinyin)
-        #         if year == 2020 & month == 4:
-        #             break
-        #     conn.commit()
+        # for month in range(1, 5):
+        #
+        #     y_m = '2020' + str(month).zfill(2)
+        #     url = 'http://www.tianqihoubao.com/lishi/' + citypinyin + '/month/' + y_m + '.html'
+        #     get_month_info(url, city)
+        #
+        #
+        # cur.execute("delete from history_citylist where city = '%s'" % city)
+        # cur.execute("delete from history_citylist_2 where city = '%s'" % city)
+        # conn.commit()
